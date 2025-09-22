8edc14f5cfa8559323f562396ec987f9ec0a049f
starlix234 <gerina.leiva@gmail.com>
2025-09-22 20:44:56 -0300

    mibarrio-siexisteelrut

--- Modelo/Modelo-registro.php
+++ Modelo/Modelo-registro.php
@@ -1,24 +1,13 @@
-
 <?php
-// Recibir datos del formulario
-$primer_nombre  = $_POST['primer_nombre'];
-$segundo_nombre = $_POST['segundo_nombre'];
-$ape_paterno    = $_POST['ape_paterno'];
-$ape_materno    = $_POST['ape_materno'];
-$fecha_nac      = $_POST['fecha_nac'];
-$telefono       = $_POST['telefono'];
-$clave          = $_POST['clave'];
-$rut            = $_POST['rut'];
-$direccion      = $_POST['direccion'];
-
-// Consulta con variables
-$sql = "INSERT INTO `usuarios` 
-(`id_usuario`, `primer_nombre`, `segundo_nombre`, `ape_paterno`, `ape_materno`, `fecha_nac`, `telefono`, `clave`, `id_rol`, `rut`, `direccion`) 
-VALUES (NULL, '$primer_nombre', '$segundo_nombre', '$ape_paterno', '$ape_materno', '$fecha_nac', '$telefono', '$clave', 3, '$rut', '$direccion')";
+function insertarUsuario($conn, $primer_nombre, $segundo_nombre, $ape_paterno, $ape_materno, $fecha_nac, $telefono, $clave, $rut, $direccion) {
+    $sql = "INSERT INTO usuarios 
+            (primer_nombre, segundo_nombre, ape_paterno, ape_materno, fecha_nac, telefono, clave, id_rol, rut, direccion) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, 3, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssssssss", $primer_nombre, $segundo_nombre, $ape_paterno, $ape_materno, $fecha_nac, $telefono, $clave, $rut, $direccion);
+    
+    return $stmt->execute();
+}
 ?>
-
-
-
-
 
 
