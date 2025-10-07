fe7562d9197df26d95a89528901d295e0f305fc5
starlix234 <gerina.leiva@gmail.com>
2025-10-07 11:36:04 -0300

    bd actualizada

---
+++ Ideas template/mibarriap.sql
@@ -0,0 +1,289 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-10-2025 a las 16:30:20
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `mibarriap`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estados`
+--
+
+CREATE TABLE `estados` (
+  `id_estado` int(11) NOT NULL,
+  `estado` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `noticias`
+--
+
+CREATE TABLE `noticias` (
+  `id_noticia` int(11) NOT NULL,
+  `titulo` varchar(150) NOT NULL,
+  `descripcion_corta` varchar(255) NOT NULL,
+  `contenido` text NOT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
+  `fecha_publicacion` timestamp NOT NULL DEFAULT current_timestamp(),
+  `autor` int(11) NOT NULL,
+  `categoria` varchar(50) DEFAULT NULL,
+  `estado` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `post_proyecto`
+--
+
+CREATE TABLE `post_proyecto` (
+  `id_postulacion` int(11) NOT NULL,
+  `id_pro` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_estado` int(11) DEFAULT NULL,
+  `fecha_postulacion` date NOT NULL DEFAULT curdate(),
+  `fecha_resolucion` date DEFAULT NULL,
+  `resolucion` varchar(1000) DEFAULT NULL,
+  `comentarios` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proyectos`
+--
+
+CREATE TABLE `proyectos` (
+  `id_pro` int(11) NOT NULL,
+  `codigo_unico` varchar(20) NOT NULL,
+  `nombre_publicacion` varchar(100) NOT NULL,
+  `contenido` varchar(2000) NOT NULL,
+  `fecha_publicacion` timestamp NOT NULL DEFAULT current_timestamp(),
+  `monto_pro` int(11) NOT NULL,
+  `id_estado` int(11) DEFAULT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `extracto` varchar(100) NOT NULL,
+  `imagen` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_rol` int(11) NOT NULL,
+  `rol` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id_rol`, `rol`) VALUES
+(1, 'Admin'),
+(2, 'Jefe Junta vecinos'),
+(3, 'miembro de junta de vecinos');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `solicitudes`
+--
+
+CREATE TABLE `solicitudes` (
+  `id_soli` int(11) NOT NULL,
+  `tipo` varchar(20) NOT NULL,
+  `asunto` varchar(40) NOT NULL,
+  `descripcion` varchar(1000) NOT NULL,
+  `estado` varchar(40) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `primer_nombre` varchar(30) NOT NULL,
+  `segundo_nombre` varchar(30) NOT NULL,
+  `ape_paterno` varchar(30) NOT NULL,
+  `ape_materno` varchar(30) NOT NULL,
+  `fecha_nac` date NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `clave` varchar(20) DEFAULT NULL,
+  `id_rol` int(11) NOT NULL,
+  `rut` varchar(40) NOT NULL,
+  `direccion` varchar(35) DEFAULT NULL,
+  `foto` varchar(2000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `estados`
+--
+ALTER TABLE `estados`
+  ADD PRIMARY KEY (`id_estado`);
+
+--
+-- Indices de la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  ADD PRIMARY KEY (`id_noticia`),
+  ADD KEY `autor` (`autor`);
+
+--
+-- Indices de la tabla `post_proyecto`
+--
+ALTER TABLE `post_proyecto`
+  ADD PRIMARY KEY (`id_postulacion`),
+  ADD UNIQUE KEY `unica_postulacion_usuario_proyecto` (`id_pro`,`id_usuario`),
+  ADD KEY `id_pro` (`id_pro`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_estado` (`id_estado`);
+
+--
+-- Indices de la tabla `proyectos`
+--
+ALTER TABLE `proyectos`
+  ADD PRIMARY KEY (`id_pro`),
+  ADD UNIQUE KEY `codigo_unico` (`codigo_unico`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_estado` (`id_estado`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `solicitudes`
+--
+ALTER TABLE `solicitudes`
+  ADD PRIMARY KEY (`id_soli`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `rut` (`rut`),
+  ADD UNIQUE KEY `rut_2` (`rut`),
+  ADD KEY `id_rol` (`id_rol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `estados`
+--
+ALTER TABLE `estados`
+  MODIFY `id_estado` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  MODIFY `id_noticia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `post_proyecto`
+--
+ALTER TABLE `post_proyecto`
+  MODIFY `id_postulacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proyectos`
+--
+ALTER TABLE `proyectos`
+  MODIFY `id_pro` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `solicitudes`
+--
+ALTER TABLE `solicitudes`
+  MODIFY `id_soli` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  ADD CONSTRAINT `noticias_ibfk_1` FOREIGN KEY (`autor`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `post_proyecto`
+--
+ALTER TABLE `post_proyecto`
+  ADD CONSTRAINT `post_proyecto_ibfk_1` FOREIGN KEY (`id_pro`) REFERENCES `proyectos` (`id_pro`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `post_proyecto_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `post_proyecto_ibfk_3` FOREIGN KEY (`id_estado`) REFERENCES `estados` (`id_estado`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `proyectos`
+--
+ALTER TABLE `proyectos`
+  ADD CONSTRAINT `proyectos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `proyectos_ibfk_2` FOREIGN KEY (`id_estado`) REFERENCES `estados` (`id_estado`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `solicitudes`
+--
+ALTER TABLE `solicitudes`
+  ADD CONSTRAINT `solicitudes_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
