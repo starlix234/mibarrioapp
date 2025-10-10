c8dbcbee188f6e32ce9487bc7a55473f27b15a58
starlix234 <gerina.leiva@gmail.com>
2025-10-10 00:47:45 -0300

    xd_xd_xd_estubod_fabian

--- Vista/vista-reguistrar-vecino.php
+++ Vista/vista-reguistrar-vecino.php
@@ -3,50 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/formulario.css">
+    <link rel="stylesheet" href="../assets/css/estilo-registra-vecino.css">
     <title>Registro Vecino</title>
 </head>
 <body>
 
-<h2>Registro Vecino</h2>
 
-<form action="../Controlador/Controlador-registro-general.php" method="POST" class="formulario" enctype="multipart/form-data">
-    
-    <label for="primer_nombre">Primer Nombre:</label>
-    <input type="text" id="primer_nombre" name="primer_nombre" required>    
+<div class="contenedor">
+   
+    <form action="../Controlador/Controlador-registro-general.php" method="POST" class="formulario" enctype="multipart/form-data">
+     <h2>Registro Vecino</h2> 
+        <input type="text" class="texto" placeholder="Primer Nombre:" id="primer_nombre" name="primer_nombre" required>    
 
-    <label for="segundo_nombre">Segundo Nombre:</label>|
-    <input type="text" id="segundo_nombre" name="segundo_nombre" required>    
+        <input type="text" id="segundo_nombre" class="texto" placeholder="Segundo Nombre" name="segundo_nombre" required>    
 
-    <label for="ape_paterno">Apellido Paterno:</label>
-    <input type="text" id="ape_paterno" name="ape_paterno" required>    
+        <input type="text" id="ape_paterno" placeholder="Apellido Paterno:" name="ape_paterno" required>    
 
-    <label for="ape_materno">Apellido Materno:</label>
-    <input type="text" id="ape_materno" name="ape_materno" required>    
+        <input type="text" id="ape_materno" placeholder="Apellido Materno:" name="ape_materno" required>    
 
-    <label for="fecha_nac">Fecha de Nacimiento:</label>
-    <input type="date" id="fecha_nac" name="fecha_nac" required>    
+        <input type="date" id="fecha_nac" name="fecha_nac" placeholder="Fecha de Nacimiento" required>    
 
-    <label for="telefono">Teléfono:</label>
-    <input type="tel" id="telefono" name="telefono" required>    
+        <input type="tel" id="telefono" name="telefono" placeholder="Teléfono" required>    
 
-    <label for="rut">RUT:</label>
-    <input type="text" id="rut" name="rut" required>    
+        <input type="text" id="rut" name="rut" placeholder="RUT" required>    
 
-    <label for="correo">Correo Electrónico:</label>
-    <input type="email" id="correo" name="correo" required>    
+        <input type="email" id="correo" name="correo" placeholder="Correo Electrónico" required>    
 
-    <label for="clave">Contraseña:</label>
-    <input type="password" id="clave" name="clave" required>    
+        <input type="password" id="clave" name="clave" placeholder="Contraseña" required>    
 
-    <label for="direccion">Dirección:</label>
-    <input type="text" id="direccion" name="direccion" required>    
+        <input type="text" id="direccion" name="direccion" placeholder="Dirección" required>    
 
-    <label for="foto">Foto:</label>
-    <input type="file" id="foto" name="foto" accept="image/*" required><br><br>
-
-    <button type="submit">Registrar</button>
+        <input type="file" id="foto" name="foto" accept="image/*" required>
+        <!--<input type="submit" class="Registrar-veci" value="vecino">-->
+    <button class="registrar-veci" type="submit">Registrar</button>
 </form>
+
+</div>
 
 <script src="validador-edad-rut.js"></script>
 
